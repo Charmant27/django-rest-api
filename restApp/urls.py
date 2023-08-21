600e53d46467a5b7b7853f61e318fbd1bfb79ce2
Charmant Gatera <charmantgatera@Charmants-MacBook-Pro.local>
2023-08-21 21:15:05 +0200

    added tasks urls

--- restApp/urls.py
+++ restApp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import apiResponse
+from .views import apiResponse, taskList
 
 urlpatterns = [
     path('', apiResponse),
+    path('task-list/', taskList, name='task-list')
 ]
