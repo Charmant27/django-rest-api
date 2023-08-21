d07a9bb859528a2dfa719bffc46681277a58092a
Charmant Gatera <charmantgatera@Charmants-MacBook-Pro.local>
2023-08-21 22:01:28 +0200

    added endpoints

--- restApp/urls.py
+++ restApp/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import apiResponse, taskList
+from .views import apiResponse, taskList, singleTask, createTask, updateTask, deleteTask
 
 urlpatterns = [
     path('', apiResponse),
-    path('task-list/', taskList, name='task-list')
+    path('task-list/', taskList, name='task-list'),
+    path('single-task/<str:pk>/', singleTask, name='single-task'),
+    path('add-task/', createTask, name='add-task'),
+    path('update-task/<str:pk>/', updateTask, name='update-task'),
+    path('delete-task/<str:pk>/', deleteTask, name='delete-task')
 ]
